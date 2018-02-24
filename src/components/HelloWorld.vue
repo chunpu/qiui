ab6c79f06aa4f687d4509d2b6dd2944ff98c799b
fengtong <fengtong@360.cn>
2018-02-24 17:53:27 +0800

    use scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,9 +95,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
 h1, h2 {
   font-weight: normal;
+  display: flex;
+  color: $color-primary;
 }
 ul {
   list-style-type: none;
